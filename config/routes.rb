c3e527cd0ca00c3f33727b6533c982c464c6692f
Joe <jcarroll786@gmail.com>
2017-07-11 23:02:07 +0100

    fixed routes for real reals

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :conversations, only: [:index, :show, :destroy]
-  
-  resources :messages, only: [:new, :create]
 
   resources :users, only: [:index]
 
